c320323d920eeb4331699ef0af1705a177e7b8a6
seweryn.fornalik <la_dolce_vita@tlen.pl>
2018-03-03 13:30:53 +0100

    Implement single tx validation (first part)

--- TxHandler.java
+++ TxHandler.java
@@ -1,4 +1,9 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+
 public class TxHandler {
+
+    private UTXOPool mUtxoPool;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -6,7 +11,10 @@ public class TxHandler {
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        if (utxoPool == null) {
+            throw new IllegalArgumentException("UTXOPool argument cannot be null!");
+        }
+        mUtxoPool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -20,6 +28,9 @@ public class TxHandler {
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
+        if (tx == null || tx.getInputs().size() == 0)
+            return false;
+        return areAllOutputsInPoolNoDoubleSpend(tx) && isBalanceOK(tx);
     }
 
     /**
@@ -31,4 +42,47 @@ public class TxHandler {
         // IMPLEMENT THIS
     }
 
+    private boolean areAllOutputsInPoolNoDoubleSpend(Transaction tx) {
+        //(1) all inputs claimed by {@code tx} are in the current UTXO pool
+        //(2) the signatures on each input of {@code tx} are valid
+        //(3) no UTXO is claimed multiple times by {@code tx}
+        ArrayList<Transaction.Input> inputs = tx.getInputs();
+        if (inputs.size() > mUtxoPool.getAllUTXO().size()) return false;
+        ArrayList<UTXO> claimedInputs = new ArrayList<>();
+        for (int i = 0; i < inputs.size(); i++) {
+            Transaction.Input input = inputs.get(i);
+            byte[] signature = input.signature;
+            UTXO tmpUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            Transaction.Output output = mUtxoPool.getTxOutput(tmpUTXO);
+            if (output == null) return false;
+            PublicKey publicKey = output.address;
+            if (!mUtxoPool.getAllUTXO().contains(tmpUTXO) || claimedInputs.contains(tmpUTXO) ||
+                    !Crypto.verifySignature(publicKey, tx.getRawDataToSign(i), signature)) {
+                return false;
+            }
+            claimedInputs.add(tmpUTXO);
+        }
+        return true;
+    }
+
+    private boolean isBalanceOK(Transaction tx) {
+        //4) all of {@code tx}s output values are non-negative
+        //(5) the sum of {@code tx}s input values is greater than or equal to the sum of its output values
+        double totalOutputSum = 0;
+        double totalInputSum = 0;
+
+        for (Transaction.Output output : tx.getOutputs()) {
+            totalOutputSum += output.value;
+            if (output.value < 0) return false;
+        }
+
+        for (Transaction.Input input : tx.getInputs()) {
+            UTXO tmpUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            Transaction.Output output = mUtxoPool.getTxOutput(tmpUTXO);
+            if (output == null) return false;
+            totalInputSum += output.value;
+        }
+
+        return Double.compare(totalInputSum, totalOutputSum) >= 0;
+    }
 }
