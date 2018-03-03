14fc94a120361e936ddd12fd144ba3ebd779fe7e
seweryn.fornalik <la_dolce_vita@tlen.pl>
2018-03-03 13:30:53 +0100

    Implement solution (first attempt)

--- TxHandler.java
+++ TxHandler.java
@@ -1,5 +1,6 @@
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TxHandler {
 
@@ -19,18 +20,17 @@ public class TxHandler {
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
+     * values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
-        if (tx == null || tx.getInputs().size() == 0)
-            return false;
-        return areAllOutputsInPoolNoDoubleSpend(tx) && isBalanceOK(tx);
+        return (tx != null && tx.getInputs().size() != 0) &&
+                areAllOutputsInPoolNoDoubleSpend(tx) && isBalanceOK(tx);
     }
 
     /**
@@ -40,6 +40,39 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        ArrayList<UTXO> claimedOutputs = new ArrayList<>();
+        ArrayList<UTXO> tmpClaimedOutputs = new ArrayList<>();
+        HashMap<UTXO, Transaction.Output> utxoPoolIncrement = new HashMap<>();
+        ArrayList<Transaction> handledTransactions = new ArrayList<>();
+        //check if every tx is valid and no double spend attempt is detected
+        outerLoop:
+        for (Transaction tx : possibleTxs) {
+            tmpClaimedOutputs.clear();
+            if (isValidTx(tx)) {
+                for (Transaction.Input input : tx.getInputs()) {
+                    UTXO tmpUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+                    //check for cross-tx double spend
+                    if (claimedOutputs.contains(tmpUTXO)) continue outerLoop;
+                    tmpClaimedOutputs.add(tmpUTXO);
+                }
+                claimedOutputs.addAll(tmpClaimedOutputs);
+                for (int i = 0; i < tx.numOutputs(); i++) {
+                    UTXO utxo = new UTXO(tx.getHash(), i);
+                    utxoPoolIncrement.put(utxo, tx.getOutput(i));
+                }
+                handledTransactions.add(tx);
+            }
+        }
+        //remove all consumed UTXO's
+        for (UTXO utxo : claimedOutputs) {
+            mUtxoPool.removeUTXO(utxo);
+        }
+        //add all new UTXO's
+        for (UTXO utxo : utxoPoolIncrement.keySet()) {
+            mUtxoPool.addUTXO(utxo, utxoPoolIncrement.get(utxo));
+        }
+
+        return (Transaction[]) handledTransactions.toArray();
     }
 
     private boolean areAllOutputsInPoolNoDoubleSpend(Transaction tx) {
